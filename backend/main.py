38a747dee1477c10661f5910750eb1ded7c1eadc
J041 <46340016+J041@users.noreply.github.com>
2024-12-15 09:57:58 +0800

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -165,16 +165,6 @@ async def scan_commit(
             else:
                 output_list.append({f"{filename}": "ERR"})
 
-<<<<<<< Updated upstream
-=======
-
-            # sandbox
-            # image_name = "sandbox-container"
-            # context_path = "./sandbox/benchmarker/Dockerfile"
-            # build_container(image_name, context_path)
-            # run_container(image_name=image_name)
-
->>>>>>> Stashed changes
     # delete previous scan of this commit in the repo
     previous_scan = session.exec(select(Scan).filter(Scan.commit_sha == params.sha, Scan.repo_name == params.repo)).first()
     if previous_scan:
